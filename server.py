2d115d13cf3e178be65198a1c1a26a3bb34112d1
PMohanJ <pmohan6712@gmail.com>
2022-02-12 10:45:46 +0530

    added broadcast func to server

--- server.py
+++ server.py
@@ -22,10 +22,34 @@ def accept_incoming_connections():
         thread.start()
 
 def handle_client(client): #taking client socket as arg
+    """handling each client in separate thread and if client 
+       types 'quit' his connetion will be closed. 
+    """
     name  = client.recv(BUFSIZ).decode("utf8")
     welcome = "Welcome {}! if wanted to quit type QUIT".format(name)
     client.send(bytes(welcome, "utf8"))
     msg = "{} had joined the chat".format(name)
     broadcast(bytes(msg, "utf8"))
     clients[client] = name
+
+    while True:
+        msg = client.recv(BUFSIZ)
+        if msg != bytes("{QUIT}", "utf8"):
+            broadcast(msg, name+": ")
+        else:
+            client.send(bytes("{QUIT}", "utf8"))
+            client.close()
+            del clients[client]
+            broadcast(bytes("{} has left the chat".format(name), "utf8"))
+            break
+
+
+def broadcast(msg, prefix=""):
+    #prefix is just for name indentification
+    #this func sends msg to all clients
+    for sock in clients:
+        sock.send(bytes(prefix, "utf8")+msg)
+
+
+
     
